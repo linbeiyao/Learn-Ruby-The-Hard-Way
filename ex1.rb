2c8ced2400aee2f564766e77646f07e5d6c86627
beiyao <153424830+linbeiyao@users.noreply.github.com>
2024-02-25 16:48:26 +0800

    Add files via upload

---
+++ ex1.rb
@@ -0,0 +1,11 @@
+#puts "Hello,World!"
+#puts "hello Again"
+#puts "I like typing this."
+#puts "This is fun."
+#puts "Yay! Printing."
+#puts "I'd much rather you 'not'."
+#puts 'I "said" do not thuch this.'
+puts "This is my Ruby！！"
+
+
+puts("Ruby 中的注释使用 ‘#’ 号来表示")
